96622a9f865660d8443ed538d7259d6b5705ae4b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-30 16:11:12 +0300

    Update 03-b-6900.sh

--- Pipes/03-b-6900.sh
+++ Pipes/03-b-6900.sh
@@ -4,22 +4,17 @@ tempFile1=$(mktemp)
 tempFile2=$(mktemp)
 
 ls -t | head -n 10 > "${tempFile1}"
-ls -u | tail -n 10 > "${tempFile2}"
+ls -u | head -n 10 > "${tempFile2}"
 
-while read -ra tokens; do
-  for((i=0;i<10;i++)); do
-    echo "${tokens[$i]}" >> "${tempFile1}"
-  done
+while read -r line; do
+  echo "${line}"
 done < "${tempFile1}" 
 
-while read -ra tokens; do
-  for((i=0;i<10;i++)); do
-    echo "${tokens[$i]}" >> "${tempFile2}"
-  done
-done < "${tempFile2}" 
+echo ' '
 
-cat "${tempFile1}"
-cat "${tempFile2}"
+while read -r line; do
+  echo "${line}"
+done < "${tempFile2}"
 
 rm -r "${tempFile1}"
 rm -r "${tempFile2}"
