23a8a8b8ce35acfa154b20331bbac4715a0426b0
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-03-30 02:27:32 +0200

    Create 03-b-5300.sh

---
+++ Pipes/03-b-5300.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+tempFile=$(mktemp)
+cat /etc/passwd | cut -d ':' -f 5 | tr -s ',' ' ' | cut -d ' ' -f 1,2 1> "${tempFile}"
+
+while read -r line; do
+  size="${#line}"
+  declare -A unorderedMap
+
+  for((i=0;i<size;i++)); do
+    currentSymbol="${line:$i:1}"
+    if [ "${currentSymbol}" != " " ]; then
+      (( unorderedMap["${currentSymbol}"]++ ))
+    fi
+  done
+
+  for key in "${!unorderedMap[@]}"; do
+    if [ "${unorderedMap["${key}"]}" -eq 1 ]; then
+      echo "${line}"
+      echo ' ' has "${key}" as uniqe symbol
+    fi
+  done
+done < "${tempFile}"
+
+rm -r tempFile
