49b5d24bc1cebeb24f76b8925ee3980aa345eac6
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-03 18:53:34 +0300

    Update 2016-SE-02.sh

--- Shell/2016-SE-02.sh
+++ Shell/2016-SE-02.sh
@@ -1,1 +1,41 @@
+#!/bin/bash
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+tempFile=$(mktemp)
+ps -e -o uid,rss | tr -s ' ' ' ' | sort -t ' ' -k 1 > "${tempFile}"
+read -p : argument
+
+currentUser=""
+currentSum=0
+
+while read -r line; do
+    newCurrentUser=$(echo "${line}" | cut -d ' ' -f 1)
+    newCurrentSum=$(echo "${line}" | cut -d ' ' -f 2)
+
+    if [[ "${currentUser}" != "${newCurrentUser}" ]]; then
+
+        if [ "${currentSum}" -gt "${argument}" ]; then
+                echo "User ${currentUser}: total RSS ${currentSum} > ${argument}"
+            else
+                echo "User ${currentUser}: total RSS ${currentSum} <= ${argument}"
+            fi
+        currentSum=0
+        currentUser=""
+
+    fi
+
+    currentSum=$(( currentSum + newCurrentSum ))
+
+done < "${tempFile}"
+
+if [ "${currentSum}" -gt "${argument}" ]; then
+    echo "User ${currentUser}: total RSS ${currentSum} > ${argument}"
+else
+    echo "User ${currentUser}: total RSS ${currentSum} <= ${argument}"
+fi
+
+rm -r "${tempFile}"
