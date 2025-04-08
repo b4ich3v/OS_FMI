7148b29e463f2b1458f964088b02a049cb2bbd0b
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-08 11:20:59 +0300

    Create 2016-SE-01.sh

---
+++ Shell/2016/2016-SE-01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Error"
+    exit 1
+fi
+
+if [ ! -d $1 ]; then
+    echo "Error"
+    exit 1
+fi
+
+dir=$1
+find "${dir}" -type l -exec test ! -e {} print ';'
