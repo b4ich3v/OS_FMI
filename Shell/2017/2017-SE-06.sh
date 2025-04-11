fc9a2528e4cdc18cf55fec90216c62e3e408a244
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-11 15:20:46 +0300

    Update 2017-SE-06.sh

--- Shell/2017/2017-SE-06.sh
+++ Shell/2017/2017-SE-06.sh
@@ -8,25 +8,28 @@ fi
 homeDirPattern="/home"
 infoFile=$(mktemp)
 resultUsers=$(mktemp)
+
 cat /etc/passwd > "${infoFile}"
 cat "${infoFile}" | grep -v "${homeDirPattern}" >> "${resultUsers}"
 
 while read -r currentLine; do
     currentUser=$(echo "${currentLine}" | awk -F ':' '{print $1}')
     currentHomeDir=$(echo "${currentLine}" | awk -F ':' '{print $6}')
-
+    
     if [ "$(stat -c %U "${currentHomeDir}")" == "${currentUser}" ]; then
-        echo "${currentLine}" | awk -F ':' '{print $1}' >> "${resultUsers}"
+        echo "${currentUser}" >> "${resultUsers}"
     fi
 
-    if ! sudo -u "${currentUser}" -w "${currentHomeDir}"; then
-        echo "${currentLine}" | awk -F ':' '{print $1}' >> "${resultUsers}"
+    if ! sudo -u "${currentUser}" ls "${currentHomeDir}" &>/dev/null; then
+        echo "${currentUser}" >> "${resultUsers}"
     fi
 
 done < "${infoFile}"
 
 dataFile=$(mktemp)
-ps -e -o uid,pid,rss | tr -s ' ' | sort -n -t ' ' -k -f 1 | tail -n +2 > "${dataFile}"
+ps -e -o uid,pid,rss --no-headers | tr -s ' ' | sort -n -k1,1 | tail -n +2 > "${dataFile}"
+
+
 prevUser=$(head -n 1 "${dataFile}" | cut -d ' ' -f 1)
 counterForRSS=0
 rootRSS=0
@@ -39,21 +42,19 @@ while read -r currentLine; do
         rootRSS=$(( rootRSS + currentRSS ))
     fi
 
-done < "${fileData}"
+done < "${dataFile}"  
 
 while read -r currentLine; do
     currentUser=$(echo "${currentLine}" | cut -d ' ' -f 1)
     currentPID=$(echo "${currentLine}" | cut -d ' ' -f 2)
     currentRSS=$(echo "${currentLine}" | cut -d ' ' -f 3)
     
-    if cat "${resultUsers}" | grep "${currentUser}"; then
-
+    if grep -q "^${currentUser}$" "${resultUsers}"; then
         if [ "${currentUser}" == "${prevUser}" ]; then
-        
-            if [ "${counterForRSS}" -gt  "${rootRSS}" ]; then
+            if [ "${counterForRSS}" -gt "${rootRSS}" ]; then
                 kill "${currentPID}"
             fi  
-
+       
             counterForRSS="${currentRSS}"
             prevUser="${currentUser}"
 
