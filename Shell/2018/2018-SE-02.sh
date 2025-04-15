84e3508c11acba0b1e0a3b1e2ba10b9e8e6cc6f8
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-04-15 17:03:37 +0300

    Update 2018-SE-02.sh

--- Shell/2018/2018-SE-02.sh
+++ Shell/2018/2018-SE-02.sh
@@ -23,13 +23,15 @@ cd "${inputDir}"
 touch "${resultDictFile}"
 
 fileWithUserFields=$(mktemp)
-cat "${inputFile}" | cut -d ' ' -f 1,2 > "${fileWithUserFields}"
+cat "${inputFile}" | tr -s ' ' | cut -d ' ' -f 1,2 | sort > "${fileWithUserFields}"
 
 prevUser=$(cat "${fileWithUserFields}" | head -n 1)
 staticVariableMask=0
+echo "${prevUser};${staticVariableMask}" >> "${resultDictFile}"
+touch "${staticVariableMask}.txt"
 
 while read -r currentUserLine; do
-    currentUser=$(echo "${currentUserLine}" | tr -s ' ' | cut -d ' ' -f 1,2)
+    currentUser=$(echo "${currentUserLine}")
 
     if [ "${currentUser}" != "${prevUser}" ]; then
         echo "${currentUser};${staticVariableMask}" >> "${resultDictFile}"
@@ -46,7 +48,7 @@ while read -r currentLine; do
     currentSentance=$(echo "${currentLine}" | tr -s ' ' | tr ': ' ':' | tr ' :' ':' | cut -d ':' -f 2)
 
     if cat "${resultDictFile}" | grep -E "${currentUser}"; then
-        idNumber=$(cat "${resultDictFile}" | grep -E "${currentUser}" | cut -d ';' -f 2 > "${idNumber}")
+        idNumber=$(grep -E "^${currentUser};" "${resultDictFile}" | cut -d ';' -f 2)
         echo "${currentSentance}" >> "${idNumber}.txt"
     fi
 
