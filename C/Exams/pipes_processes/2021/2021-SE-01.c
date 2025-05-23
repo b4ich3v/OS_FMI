7827c67ed44cd32b69b984a97e4efe01504074c4
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-23 13:32:34 +0300

    Create  2021-SE-01.c

---
+++ C/Exams/pipes_processes/2021/2021-SE-01.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <err.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) 
+{
+
+    if(argc != 3) errx(1, "Error");
+
+    int fd = open(argv[1], O_RDONLY);
+    if(fd < 0) errx(1, "Error");
+
+    uint8_t currentByte = 0;
+    uint8_t dummyTrash = 0;
+    uint16_t operatingMaskManipulation = 1;
+
+    uint8_t helper1 = 0b01;
+    uint8_t helper2 = 0b10;
+
+    while((dummyTrash = read(fd, &currentByte, sizeof(uint8_t))) > 0)
+    {
+
+        uint16_t reversedMask = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            
+            if(currentByte & (operatingMaskManipulation < i))
+            {
+
+                reversedMask |= (helper1 << (2 * i));
+
+            }
+            else
+            {
+                
+                reversedMask |= (helper2 << (2 * i));
+
+            }
+
+        }
+     
+        uint16_t result = 0;
+        result |= (reversedMask << 4);
+        result |= (reversedMask >> 4);
+        write(1, &result, sizeof(uint16_t));
+
+    }
+    
+    close(fd);
+    exit(0);
+
+}
