827256e1fafd7fce8c7ccf104096e35972ca50fe
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-05-23 13:45:58 +0300

    Update 2021-SE-02.c

--- C/Exams/pipes_processes/2021/2021-SE-02.c
+++ C/Exams/pipes_processes/2021/2021-SE-02.c
@@ -1,1 +1,61 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <err.h>
+#include <sys/wait.h>
 
+int main(int argc, char *argv[]) 
+{
+
+    if(argc != 3) errx(1, "Error");
+    
+    int fd1 = open(argv[1], O_RDONLY);
+    if(fd1 < 0) errx(1, "Error");
+
+    int fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if(fd2 < 0) errx(1, "Error");
+    
+    uint8_t currentByte = 0;
+    uint8_t mask1 = 0b10;
+    uint8_t mask2 = 0b01;
+
+    while(read(fd1, &currentByte, sizeof(uint8_t)) > 0)
+    {
+
+        uint16_t currentTwoBytesMask = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            
+            if(currentByte & (1 << i))
+            {
+
+                currentTwoBytesMask |= (mask1 << (2 * i));
+
+            }
+            else
+            {
+            
+                currentTwoBytesMask |= (mask2 << (2 * i));
+
+            }
+
+        }
+        
+        uint16_t result = 0;
+        result |= (currentTwoBytesMask >> 4);
+        result |= (currentTwoBytesMask << 4);
+
+        write(fd2, &result, sizeof(uint8_t));
+
+    }
+
+    close(fd1);
+    close(fd2);
+    exit(0);
+
+}
