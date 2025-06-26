4d35afe1fc1251a3ebd9bf3217d6803e85fb3361
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-06-26 15:30:13 +0300

    Update 2016-SE-02.c

--- C/Exams/input_output/2016/2016-SE-02.c
+++ C/Exams/input_output/2016/2016-SE-02.c
@@ -1,12 +1,16 @@
-#include "stdio.h"
-#include "stdint.h"
-#include "stdlib.h"
-#include "fcntl.h"  
-#include "string.h"
-#include "unistd.h"
-#include <err.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <stdint.h>
 
-typedef struct
+typedef struct Pair
 {
 
     uint32_t number1;
@@ -14,77 +18,124 @@ typedef struct
 
 } Pair;
 
-off_t getFileSize(int fd)
+int main(int argc, char *argv[])
 {
 
-    off_t currentPos = lseek(fd, 0, SEEK_CUR);
-    if(currentPos < 0) { write(2, "Error\n", 6); exit(1); }
-    
-    off_t result = lseek(fd, 0, SEEK_END);
-    if(result < 0) { write(2, "Error\n", 6); exit(1); }
+    if(argc != 4) errx(1, "Error");
 
-    if(lseek(fd, currentPos, SEEK_SET) < 0) { write(2, "Error\n", 6); exit(1); }
+    int fd1 = open(argv[1], O_RDONLY);
+    if(fd1 < 0) errx(1, "Error");
 
-    return result;
+    int fd2 = open(argv[2], O_RDONLY);
+    if(fd2 < 0) errx(1, "Error");
 
-}
+    int fd3 = open(argv[3], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if(fd3 < 0) errx(1, "Error");
 
-void freeMethodErr(int fd1, int fd2, int fd3)
-{
+    Pair currentPair;
+    struct stat st1, st2;
 
-    write(2, "Error\n", 6);
+    if(fstat(fd1, &st1) < 0) err(1, "Error");
+    if(fstat(fd2, &st2) < 0) err(1, "Error");   
 
-    close(fd1);
-    close(fd2);
-    close(fd3);
+    ssize_t sizeOfFile1 = st1.st_size;
+    ssize_t sizeOfFile2 = st2.st_size;
 
-    exit(1);
-
-}
-
-int main(int argc, const char* argv[])
-{
-
-    if(argc != 4)
+    if(sizeOfFile1 % 8 != 0)
     {
 
-        errx(2, "Error\n");
-        exit(1);
+        close(fd1);
+        close(fd2);
+        close(fd3);
+        err(1, "Error");
 
     }
 
-    int fd1 = open(argv[1], O_RDONLY);
-    int fd2 = open(argv[2], O_RDONLY);
-    int fd3 = open(argv[3], O_WRONLY | O_TRUNC | O_CREAT, 0666);
-
-    off_t fileSize = getFileSize(fd1);
-    int countOfPairs = fileSize / 8;
-    Pair pair;
-
-    if(fileSize % 8 != 0) freeMethodErr(fd1, fd2, fd3);
-
-    for (int i = 0; i < countOfPairs; i++)
+    while(read(fd1, &currentPair, sizeof(Pair)) > 0)
     {
 
-        if(read(fd1, &pair, sizeof(Pair)) < 0) freeMethodErr(fd1, fd2, fd3);
-        
-        off_t wantedPos = lseek(fd2, (off_t)pair.number1 * sizeof(uint32_t), SEEK_SET);
-        if(wantedPos < 0) freeMethodErr(fd1, fd2, fd3);
+        uint32_t startPosition = currentPair.number1;
+        uint32_t offset = currentPair.number2;
 
-        uint32_t value = 0;
-
-        for (int j = 0; j < pair.number2; j++)
+        if(lseek(fd2, startPosition * sizeof(uint32_t), SEEK_SET) < 0)
         {
-            
-            if(read(fd2, &value, sizeof(uint32_t)) < 0) freeMethodErr(fd1, fd2, fd3);
-            if(write(fd3, &value, sizeof(uint32_t)) < 0 ) freeMethodErr(fd1, fd2, fd3);
+
+            close(fd1);
+            close(fd2);
+            close(fd3);
+            err(1, "Error");
 
         }
 
-        if(lseek(fd2, 0, SEEK_SET)) freeMethodErr(fd1, fd2, fd3);
+        if((startPosition + offset) * sizeof(uint32_t) > sizeOfFile2)
+        {
 
+            close(fd1);
+            close(fd2);
+            close(fd3);
+            err(1, "Error");
+
+        }
+        
     }
-    
+
+    if(lseek(fd1, 0, SEEK_SET) < 0) err(1, "Error");
+
+    while(read(fd1, &currentPair, sizeof(Pair)) > 0)
+    {
+
+        uint32_t startPosition = currentPair.number1;
+        uint32_t offset = currentPair.number2;
+
+        if(lseek(fd2, startPosition * sizeof(uint32_t), SEEK_SET) < 0)
+        {
+
+            close(fd1);
+            close(fd2);
+            close(fd3);
+            err(1, "Error");
+
+        }
+
+        if((startPosition + offset) * sizeof(uint32_t) > sizeOfFile2)
+        {
+
+            close(fd1);
+            close(fd2);
+            close(fd3);
+            err(1, "Error");
+
+        }
+
+        for (int i = 0; i < offset; i++)
+        {
+            
+            uint32_t readByte = 0;
+
+            if(read(fd2, &readByte, sizeof(uint32_t)) < 0)
+            {
+
+                close(fd1);
+                close(fd2);
+                close(fd3);
+                err(1, "Error");
+
+            }
+
+            if(write(fd3, &readByte, sizeof(uint32_t)) < 0)
+            {
+
+                close(fd1);
+                close(fd2);
+                close(fd3);
+                err(1, "Error");
+
+            }
+
+        }
+        
+    }
+
     close(fd1);
     close(fd2);
     close(fd3);
