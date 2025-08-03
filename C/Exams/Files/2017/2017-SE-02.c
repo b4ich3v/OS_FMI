7a9e2c54311350d0bed7389b677f7067643c66d7
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-03 17:40:03 +0300

    Create 2017-SE-02.c

---
+++ C/Exams/Files/2017/2017-SE-02.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <err.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	
+	bool hasNumbers = false;
+	if(!strcmp(argv[1], "-n")) hasNumbers = true;
+
+	for (int i = 1; i < argc; i++)
+	{
+
+		if(hasNumbers && i == 1) i += 1;
+		const char* currentArg = argv[i];
+
+		if(strcmp(currentArg, "-"))
+		{
+
+			int readingFd = open(currentArg, O_RDONLY);
+			if(readingFd < 0) err(1, "Open error");
+
+			char currentByte = 'a';
+			char buffer[1024] = {0};
+			int index = 0;
+
+			while(read(readingFd, &currentByte, sizeof(char)) > 0)
+			{
+
+				buffer[index] = currentByte;
+				index += 1;
+
+			}
+
+			buffer[index] = '\0';
+
+			if(hasNumbers)
+			{
+
+				char result[1024] = {0};
+				if(snprintf(result, sizeof(result), "%d %s\n", i - 1, buffer) < 0) err(1, "Snprintf error");
+				if(write(1, result, strlen(result)) < 0) err(1, "Write error");
+
+			}
+			else
+			{
+
+				if(write(1, buffer, strlen(buffer)) < 0) err(1, "Write error");
+
+			}
+
+			close(readingFd);
+
+		}
+		else
+		{
+
+			char currentByte = 'a';
+			char buffer[1024] = {0};
+			int index = 0;
+
+			while(read(0, &currentByte, sizeof(char)) > 0)
+			{
+
+				if(currentByte == '\n') break;
+				buffer[index] = currentByte;
+				index += 1;
+
+			}
+
+			buffer[index] = '\0';
+
+			if(hasNumbers)
+			{
+
+				char result[1024] = {0};
+				if(snprintf(result, sizeof(result), "%d %s\n", i - 1, buffer) < 0) err(1, "Snprintf error");
+				if(write(1, result, strlen(result)) < 0) err(1, "Write error");
+
+			}
+			else
+			{
+
+				if(write(1, buffer, strlen(buffer)) < 0) err(1, "Write error");
+
+			}
+
+		}
+
+	}
+	
+	return 0;
+
+}
+
