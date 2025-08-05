7548ddee08fef81d9ff775681091c77219e89f13
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-05 09:56:43 +0300

    Create 2021-SE-02.c

---
+++ C/Exams/Files/2021/2021-SE-02.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <err.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[])
+{
+
+	if(argc != 3) errx(1, "Wrong number of arguments");
+
+	int readingFd = open(argv[1], O_RDONLY);
+	if(readingFd < 0) err(1, "Open error"); 
+
+	int writingFd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if(writingFd < 0) err(1, "Open error");
+
+	uint16_t currentNumber = 0;
+
+	while(read(readingFd, &currentNumber, sizeof(uint16_t)) > 0)
+	{
+
+		uint8_t currentByte = 0;
+
+		for(int i = 7; i >= 0; i--)
+		{
+
+			uint8_t currentPair = 0;
+			currentPair = (currentNumber >> (2 * i)) & 3;
+
+			if(currentPair == 0x10)
+			{
+
+				currentByte |= (1 << i);
+
+			}
+
+		}	
+
+		if(write(writingFd, &currentNumber))
+
+	}
+
+	close(readingFd);
+	close(writingFd);
+	return 0;
+
+}
+
