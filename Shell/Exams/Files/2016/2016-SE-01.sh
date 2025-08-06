249f4d3d1a270bbd0025ebd137a96eb62adf0fa3
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-06 16:29:28 +0300

    Update 2016-SE-01.sh

--- Shell/Exams/Files/2016/2016-SE-01.sh
+++ Shell/Exams/Files/2016/2016-SE-01.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Wrong number of arguments"
+	exit 1
+fi	
+
+tempFile=$(mktemp)
+cat "$1" 1> "$tempFile" 2> "/dev/null"
+
+while IFS= read -r currentLine; do
+	if echo "$currentLine" | grep -q '[02468]' && ! echo "$currentLine" | grep -q '[a-z]'; then
+        printf "%s\n" "$currentLine"
+    fi
+done < "$tempFile"
+
+rm -r "$tempFile"
+exit 0
+
+
 
