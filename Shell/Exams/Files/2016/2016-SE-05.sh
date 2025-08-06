27d6e804e31dce5225de9d6d2686d9a5e2c49296
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-06 23:38:37 +0300

    Create  2016-SE-05.sh

---
+++ Shell/Exams/Files/2016/2016-SE-05.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ] || [ ! -f "$1" ] || [ ! -f "$2" ]; then
+	echo "Wrong number of arguments or does'n exist"
+	exit 1
+fi
+
+counter1=$(cat "$1" | grep -E "$1" | wc -l)
+counter2=$(cat "$2" | grep -E "$2" | wc -l)
+firstHasMore=$(echo "$counter1 > $counter2" | bc)
+executingFile="OK"
+
+if [ "$firstHasMore" -eq 1 ]; then
+	executingFile="$1"	
+else
+	executingFile="$2"
+fi
+
+cat "$executingFile" | cut -d '-' -f2 | sed -E "s/[\t\r ]//" | sort
+
+exit 0
