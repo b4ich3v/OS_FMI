e646a6866ac9a49755ad56327a7588b4a787ddf7
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-06 23:15:14 +0300

    Create 2016-SE-03.sh

---
+++ Shell/Exams/Files/2016/2016-SE-03.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# if [ "$(id -u)" -ne 0 ] ; then
+# 	echo "Inalid argument"
+# 	exit 1
+# fi
+
+cat /etc/passwd | awk -F ':' '{
+	found=0
+
+	for(i=1;i<=NF;i++)
+	{
+
+		if($i ~ /home/)
+		{
+
+			found=1
+			break
+
+		}
+		
+	}
+
+	if(!found) { print $0 }
+}'
+
+exit 0
