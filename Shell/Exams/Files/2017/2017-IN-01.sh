8db536a9b8da8916d50b79e5eec9ecb6248ed928
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-06 16:23:13 +0300

    Update 2017-IN-01.sh

--- Shell/Exams/Files/2017/2017-IN-01.sh
+++ Shell/Exams/Files/2017/2017-IN-01.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+
+userId=$(id -u)
+tempFile=$(mktemp)
+
+find ~ -type f -exec stat -c "%u %n" {} ';' 1> "$tempFile" 2> "/dev/null"
+
+while IFS= read -r currentLine; do
+	id=$(echo "$currentLine" | cut -d ' ' -f1)
+	fileName=$(echo "$currentLine" | cut -d ' ' -f2)
+	
+	if [ "$id" -ne "$userId" ]; then
+		continue
+	fi
+
+	printf "%s\n" "$fileName"
+
+done < "$tempFile"
+
+
+rm -r "$tempFile"
+
 
