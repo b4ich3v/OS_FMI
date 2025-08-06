2174377ac3e305a3f796eb7c78615fcf6c92868e
Yoan Baychev <153004069+b4ich3v@users.noreply.github.com>
2025-08-06 16:15:58 +0300

    Create  2017-IN-02.sh

---
+++ Shell/Exams/Files/2017/2017-IN-02.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -gt 0 ]; then
+	echo "Wrong number of arguments"
+	exit 1
+fi
+
+if [ -d "$1" ]; then
+	echo "Directory does not exist"
+	exit 1
+fi
+
+homeDir=$(echo "$HOME")
+
+tempFile=$(mktemp)
+find "$homeDir" -type f -exec stat -c "%s %n" {} ';' 1> "$tempFile"
+
+while IFS= read -r currentLine; do
+	countOfBytes=$(echo "$currentLine" | cut -d ' ' -f1)
+	fileName=$(echo "$currentLine" | cut -d ' ' -f2)
+
+	if [ "$countOfBytes" -eq 0 ]; then
+		printf "$fileName\n"
+	fi
+
+done < "$tempFile"
+
+rm -r "$tempFile"
+
+find "$homeDir" -type f -exec stat -c "%s %n" {} ';' | sort -t ' ' -k2 -n | cut -d ' ' -f2
+printf "\n"
+find "$homeDir" -type d -exec stat -c "%s %n" {} ';' | sort -t ' ' -k2 -n | cut -d ' ' -f2
+
+exit 1
